649cceae9de77c18fe1cefe709816ffd4a93d054
MarcusYim <marcusyim2004@gmail.com>
2021-09-21 10:29:12 -0700

    Revert "is this better"
    This reverts commit 8eefcc7d0139fb615147543f9dba079c6c2a49af.

--- BetaOne/BetaOne.py
+++ BetaOne/BetaOne.py
@@ -2,6 +2,11 @@ import chess
 
 def main():
     board = chess.Board()
+    board.push_san("e4")
+    board.push_san("e5")
+    board.push_san("Nf3")
+    board.push_san("f5")
+    board.push_san("g4")
     print(board);
     
     for i in board.legal_moves:
